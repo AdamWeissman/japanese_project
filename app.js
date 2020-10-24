083989e4870f9266e116ad135f191829e7ee33b4
AdamWeissman <adam@comedytragedyepic.com>
2020-10-23 22:59:57 -0400

    tree nodes can be used for sorta choose your own adventure memorization... left left for positive responses, right for negative ones

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 testingThing = document.getElementById('TEST')
-testingThing.addEventListener('click', () => {
-  testingThing.innerHTML = sample_arr['Phrases'][0][0]
+testingThing.addEventListener('click', async () => {
+  // testingThing.innerHTML = sample_arr['Phrases'][0][0]
+  console.log(testing[0])
+  console.log(testing[1])
+  console.log(testing[2])
+
 })
---
+++ japaneseNodes.js
@@ -0,0 +1,17 @@
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const root = new TreeNode("How are you? ");
+root.left = new TreeNode("I'm good.")
+root.right = new TreeNode("I'm terrible.")
+root.left.left = new TreeNode("That's great to hear.")
+root.left.right = new TreeNode("I'm glad you're doing well")
+root.right.left = new TreeNode("I'm sorry to hear that...")
+root.right.right = new TreeNode("BOO HOO.")
+
+const testing = [root, root.left, root.left.left]
