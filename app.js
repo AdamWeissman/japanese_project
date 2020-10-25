408bb850c24b2b2d2424b8fbe0e22490a4de36bf
AdamWeissman <adam@comedytragedyepic.com>
2020-10-25 15:18:15 -0400

    setThePrompt as a message

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 thePrompt = document.getElementById('thePrompt')
-thePrompt.innerHTML = testing[0].val
+
+function setThePrompt(withThisMessage) {
+  thePrompt.innerHTML = withThisMessage
+}
+
 
 userInputForm = document.getElementById('userInputSlot')
 userInputForm.addEventListener('click', async () => {
   // testingThing.innerHTML = sample_arr['Phrases'][0][0]
-  thePrompt.innerHTML = testing[1].val[2]
+  setThePrompt(testing[0].val)
+  //thePrompt.innerHTML = testing[1].val[2]
   console.log(testing[1].val[2]) // this is a little convoluted, but we can pick from the array
   console.log(testing[2])
 
