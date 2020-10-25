0f61a9372a40c7f39fbd7cf25f83815d25d1ea56
AdamWeissman <adam@comedytragedyepic.com>
2020-10-25 15:08:41 -0400

    trying to cycle

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-testingThing = document.getElementById('TEST')
-testingThing.addEventListener('click', async () => {
+thePrompt = document.getElementById('thePrompt')
+thePrompt.innerHTML = testing[0].val
+
+userInputForm = document.getElementById('userInputSlot')
+userInputForm.addEventListener('click', async () => {
   // testingThing.innerHTML = sample_arr['Phrases'][0][0]
-  console.log(testing[0])
+  thePrompt.innerHTML = testing[1].val[2]
   console.log(testing[1].val[2]) // this is a little convoluted, but we can pick from the array
   console.log(testing[2])
 
