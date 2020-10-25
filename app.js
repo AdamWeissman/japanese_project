1f47d35493fe836cb2568e6c89c050ac7a76c0cc
AdamWeissman <adam@comedytragedyepic.com>
2020-10-25 15:21:57 -0400

    working towards user input submission

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ function setThePrompt(withThisMessage) {
   thePrompt.innerHTML = withThisMessage
 }
 
+inputMessage = document.getElementById('inputMessage')
+
+function setInputMessage(withThisMessage) {
+  inputMessage.innerHTML = withThisMessage
+}
 
 userInputForm = document.getElementById('userInputSlot')
+
+function submitUserInput(userInput) {
+
+}
+
 userInputForm.addEventListener('click', async () => {
-  // testingThing.innerHTML = sample_arr['Phrases'][0][0]
   setThePrompt(testing[0].val)
   //thePrompt.innerHTML = testing[1].val[2]
   console.log(testing[1].val[2]) // this is a little convoluted, but we can pick from the array
