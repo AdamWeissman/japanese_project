c929c3d81b2e14a9d4667c7850d7b2668a0a3b1f
AdamWeissman <adam@comedytragedyepic.com>
2020-10-25 19:48:59 -0400

    sample css loaded

--- appOld.js
+++ appOld.js
@@ -1,20 +1,20 @@
-thePrompt = document.getElementById('thePrompt') // top prompt or root value of node
-inputMessage = document.getElementById('inputMessage') // for error messages etc
-theFormButton = document.getElementById('submitButton')
+// thePrompt = document.getElementById('thePrompt') // top prompt or root value of node
+// inputMessage = document.getElementById('inputMessage') // for error messages etc
+// theFormButton = document.getElementById('submitButton')
 
 
-function setThePrompt(withThisMessage) {
-  thePrompt.innerHTML = withThisMessage
-}
+// function setThePrompt(withThisMessage) {
+//   thePrompt.innerHTML = withThisMessage
+// }
 
-function setInputMessage(withThisMessage) {
-  inputMessage.innerHTML = withThisMessage
-}
+// function setInputMessage(withThisMessage) {
+//   inputMessage.innerHTML = withThisMessage
+// }
 
-function submitUserInput() {
-  let thang = document.getElementById('userActualInput').value;
-  console.log("this worked: ", thang);
-}
+// function submitUserInput() {
+//   let thang = document.getElementById('userActualInput').value;
+//   console.log("this worked: ", thang);
+// }
 
 
 // userInputForm.addEventListener('click', async () => {
