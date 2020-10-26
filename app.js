59bf7c10703bb40d598b41d30badac5a9c89dad5
AdamWeissman <adam@comedytragedyepic.com>
2020-10-25 20:59:06 -0400

    need to setup to wipe out text in submission box

--- app.js
+++ app.js
@@ -19,57 +19,22 @@ function showSuccess(input) {
 // Check userResponse is not null
 function checkUserResponseNotEmpty(input) {
   //const reForEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (input.value.length() > 0 ) {
+  if (input.value.length > 0 ) {
     showSuccess(input);
   } else {
-    showError(input, 'Enter your response!!!');
+    showError(input, 'YOU GOTTA ENTER SOMETHING!!!');
   }
 }
 
 // check user response matches something in node Array
 
 
-// Check required fields
-function checkRequired(inputArr) {
-  inputArr.forEach(function(input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
 
-// Check input length
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
-  }
-}
-
-
-// Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
 
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  checkUserResponseNotEmpty(input);
-  // checkLength(username, 3, 15);
-  // checkLength(password, 6, 25);
-  // checkEmail(email);
-  // checkPasswordsMatch(password, password2);
+  checkUserResponseNotEmpty(userResponse);
+
 });
