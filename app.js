c096ef7d000f36922bea562d494b6fc7e41c93bc
AdamWeissman <adam@comedytragedyepic.com>
2020-10-25 22:10:58 -0400

    hard coding tree traversal for now, but seems to work

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const form = document.getElementById('form');
 const thePrompt = document.getElementById('prompt')
 const userResponse = document.getElementById('userResponse');
 
+
+thePrompt.value = jNodes[0].val
+
 // Show input error message
 function showError(input, message) {
   const formControl = input.parentElement;
@@ -20,13 +23,21 @@ function showSuccess(input) {
 function properResponse(input) {
   if (input.value == jNodes[1].val[0]) {
     showSuccess(input);
-    thePrompt.innerHTML = jNodes[2].val
-  } else {
+    thePrompt.value = jNodes[2].val
+    clearUserResponse()
+  } else if (input.value == jNodes[3].val[0]) {
+    showSuccess(input);
+    thePrompt.value = jNodes[4].val
+    clearUserResponse()
+  }
+    else {
     showError(input, 'WRONG-O');
   }
 }
 
-thePrompt.innerText = jNodes[0].val
+function clearUserResponse() {
+  userResponse.value = ""
+}
 
 // Event listeners
 form.addEventListener('submit', function(e) {
--- japaneseNodes.js
+++ japaneseNodes.js
@@ -8,10 +8,14 @@ class conversationNode {
 
 const howAreYou = new conversationNode("How are you?");
 howAreYou.left = new conversationNode(["I'm good.", "I'm great!", "I'm wonderful!"]) // this would give a random response
+howAreYou.left.left = new conversationNode("That's great to hear. How's the family?")
+howAreYou.left.left.left = new conversationNode(["They're awesome!", "They're doing great, thank you for asking!"])
+howAreYou.left.left.left.left = new conversationNode("Fantastic!")
+
+
 howAreYou.right = new conversationNode("I'm terrible.")
-howAreYou.left.left = new conversationNode("That's great to hear.")
 howAreYou.left.right = new conversationNode("I'm glad you're doing well")
 howAreYou.right.left = new conversationNode("I'm sorry to hear that...")
 howAreYou.right.right = new conversationNode("BOO HOO.")
 
-const jNodes = [howAreYou, howAreYou.left, howAreYou.left.left]
+const jNodes = [howAreYou, howAreYou.left, howAreYou.left.left, howAreYou.left.left.left, howAreYou.left.left.left.left]
